e60e3b9679d8265ad7e5e3586b499108dff69056
Simone Abbamonte <simone@Simones-MacBook-Air.local>
2022-03-23 11:27:01 -0400

    finished restaurant.html

--- Reviews/views.py
+++ Reviews/views.py
@@ -9,6 +9,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from statistics import mean
+from django.core.paginator import Paginator
 
 from .models import User, Info, Review
 
@@ -17,12 +18,15 @@ from .models import User, Info, Review
 @csrf_exempt
 def index(request):
     if request.method == 'GET':
-        reviews = Review.objects.all()
+        reviews = Review.objects.all().order_by('-id')
+        paginator = Paginator(reviews, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
         try:
             info = Info.objects.get(user=request.user)
-            return render(request, 'reviews/index.html', {"info": info, "reviews": reviews})
+            return render(request, 'reviews/index.html', {"info": info, "reviews": reviews, "page_obj": page_obj})
         except ObjectDoesNotExist:
-            return render(request, 'reviews/index.html', {"reviews": reviews})
+            return render(request, 'reviews/index.html', {"reviews": reviews, "page_obj": page_obj})
             
 # Try to log user in
 @csrf_exempt
@@ -152,8 +156,11 @@ def review(request):
 def restaurant(request, restaurant):
     if request.method == "GET":
         all_reviews = Review.objects.filter(name=restaurant)
+        paginator = Paginator(all_reviews, 3)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
         four_day_week = Review.objects.filter(name=restaurant, days=4)
-        server_total_pay, bar_total_pay, buss_total_pay, run_total_pay, back_total_pay, envo, mngmt = [], [], [], [], [], [], []
+        server_total_pay, bar_total_pay, buss_total_pay, run_total_pay, back_total_pay, envo, mngmt, hours, rating = [], [], [], [], [], [], [], [], []
 
         for review in four_day_week:
             if review.position == 'SERVER':
@@ -170,12 +177,13 @@ def restaurant(request, restaurant):
         for review in all_reviews:
             envo.append(review.envo)
             mngmt.append(review.mngmt)
+            rating.append(review.rating)
+            hours.append(review.hours)
 
         server_average_pay, bar_average_pay, buss_average_pay, run_average_pay, back_average_pay = 0, 0, 0, 0, 0
 
         if server_total_pay:
             server_average_pay = mean(server_total_pay)
-            print(server_average_pay)
         if bar_total_pay:
             bar_average_pay = mean(bar_total_pay)
         if buss_total_pay:
@@ -193,8 +201,10 @@ def restaurant(request, restaurant):
             "run_average_pay": run_average_pay,
             "back_average_pay": back_average_pay,
             "average_envo": mean(envo),
-            "average_mngmt": mean(mngmt)
+            "average_mngmt": mean(mngmt),
+            "average_hours": mean(hours),
+            "average_rating": mean(rating)
         }
 
-        return render(request, 'reviews/restaurant.html', {"restaurant": restaurant, "averages": all_averages})
+        return render(request, 'reviews/restaurant.html', {"restaurant": restaurant, "averages": all_averages, "page_obj": page_obj})
 
